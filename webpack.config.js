af878c93bbaab763f192cf21a8da8970024c06f3
Pontus Sandberg <sandbergpontus97@gmail.com>
2022-09-13 22:39:41 +0200

    -

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,27 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  mode: 'development',
   module: {
     rules: [
       {
-        test: /(\.ts$|\.tsx$)/,
+        test: /\.ts$/,
         include: [path.resolve(__dirname, 'src')],
         use: 'ts-loader',
       }
     ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devServer: {
+    static: './public',
+    port: '3000',
+  },
+  devtool: 'eval-source-map',
+  output: {
+    publicPath: 'public',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
   },
 };
