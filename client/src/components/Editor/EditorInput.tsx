f69bb141d7ee3ada1db3532f43b1cbd81a536112
Pontus Sandberg <sandbergpontus97@gmail.com>
2023-03-13 17:08:59 +0100

    Parser works but not tested fully

--- client/src/components/Editor/EditorInput.tsx
+++ client/src/components/Editor/EditorInput.tsx
@@ -215,25 +215,23 @@ const EditorInput = ({
 
       return resultRow;
     }
-    // console.log('Test')
-    // console.log(
-    //   removeContentFromRow(
-    //     [{style: Settings.defaultStyles, content: '123'},{style: Settings.defaultStyles, content: 'abc'},{style: Settings.defaultStyles, content: 'xqc'}],
-    //     -2,
-    //     1,
-    //   )
-    // );
 
-
+    /**
+     * Helper to add new content to documentFile.
+     */
     const addContentToDocument = (
       rows: DocumentFile['rows'],
       selectionStartIndex: number,
       newContent: string,
     ) => {
 
+      if (newContent.length === 0) {
+        return rows;
+      };
+
       const startRowIndex = findRowIndex(rows, selectionStartIndex);
 
-      // TMP rows result.
+      // TMP for row iteraction.
       let rowsResult: DocumentFileRowRenderData[][] = [];
 
       for (const [rowIndex, row] of rows.entries()) {
@@ -241,7 +239,7 @@ const EditorInput = ({
         if (rowIndex !== startRowIndex) {
           rowsResult.push(row);
           continue;
-        }
+        };
 
         const addContentRows = newContent.split('\n');
         const addContentStartRow = addContentRows[0];
@@ -250,11 +248,20 @@ const EditorInput = ({
 
         // TMPs for section iteration.
         let startRow: DocumentFileRowRenderData[] = [];
+        let trailingRows: DocumentFileRowRenderData[][] = [];
         let prevSectionsContentLength = 0;
 
+        // sectionContentLengt + prevSectionsContentLength > rowContentStartIndex
+        // 3 + 0 > 3 - false
+        // 3 + 3 > 3  - true
+        // 'abc' '123' 'fff'
         for (const [sectionIndex, section] of row.entries()) {
+
           const isSectionBeforeStart = prevSectionsContentLength + section.content.length < rowContentStartIndex;
-          const isSectionAfterStart = prevSectionsContentLength >= rowContentStartIndex;
+          const isSectionAfterStart = prevSectionsContentLength >= rowContentStartIndex && sectionIndex > 0;
+          const sectionStartIndexOffset = rowContentStartIndex - prevSectionsContentLength;
+
+          console.log('sectionStartIndexOffset',sectionStartIndexOffset)
 
           // # Add unchanged section to startRow if it's before start index.
           if (isSectionBeforeStart) {
@@ -265,61 +272,71 @@ const EditorInput = ({
           // # Trailing sections are handled when content is added so we break the loop.
           else if (isSectionAfterStart) {
             break;
-          }
-
-          /**
-           * Trailing section content after selection start.
-           */
-          const sectionTrailingContent = section.content.substring(
-            rowContentStartIndex - prevSectionsContentLength,
-          );
+          };
 
           // # If new rows need to be added.
-          if (
-            sectionTrailingContent
-            && addContentTrailingRows.length
-          ) {
+          if (addContentTrailingRows.length) {
+
             const updatedSectionContent = (
-              section.content.substring(0, rowContentStartIndex - prevSectionsContentLength)
+              section.content.substring(0, sectionStartIndexOffset)
               + addContentStartRow
             );
 
-            // # Add current section with updated content to startRow.
-            startRow.push({
+            // Add current section with updated content to startRow.
+            const updatedSection = {
               ...section,
               content: updatedSectionContent,
-            });
+            };
 
-            // # Build trailing rows.
-            const trailingRows: DocumentFileRowRenderData[][] = addContentTrailingRows.map((trailingRowText, i) => {
+            // Build trailing rows.
+            const mappedTrailingRows: DocumentFileRowRenderData[][] = addContentTrailingRows.map((trailingRowText, i) => {
               const isLast = i === addContentTrailingRows.length - 1;
-              const trailingSectionsFromStartRow = isLast
-                ? removeContentFromRow(row, 0, rowContentStartIndex)
-                : [];
 
-              return [
-                {
-                  style: { ...Settings.defaultStyles },
-                  content: trailingRowText,
-                },
-                ...trailingSectionsFromStartRow,
-              ];
+              // For last trailing row, get sections that was "cut off" from start row.
+              let trailingSectionsFromStartRow: DocumentFileRowRenderData[] = [];
+              if (isLast) {
+                const trailingSections = removeContentFromRow(row, 0, rowContentStartIndex);
+                const trailingSectionsContentLength = trailingSections.reduce((acc, section) => acc + section.content.length, 0);
+                trailingSectionsFromStartRow = isLast && trailingSectionsContentLength
+                  ? trailingSections
+                  : [];
+              }
+
+              const newContentSections = {
+                style: { ...Settings.defaultStyles },
+                content: trailingRowText,
+              }
+
+              const newRowSections = [newContentSections, ...trailingSectionsFromStartRow];
+              const newRowContentLength = newRowSections.reduce((acc, section) => acc + section.content.length, 0);
+
+              if (newRowContentLength) {
+                return newRowSections.filter(section => section.content.length > 0);
+              } else {
+                return newRowSections.filter((_section, i) => i === 0);
+              }
             });
 
-            rowsResult = [...rowsResult, startRow, ...trailingRows];
+            startRow = [...startRow, updatedSection];
+            trailingRows = mappedTrailingRows;
           }
           // # Handle no new rows to add.
           else {
+            const trailingSectionContent = section.content.slice(sectionStartIndexOffset);
+            console.log('trailingSectionContent',trailingSectionContent)
+
             const updatedSectionContent = (
               section.content.substring(0, rowContentStartIndex - prevSectionsContentLength)
               + addContentStartRow
-              + sectionTrailingContent
+              + trailingSectionContent
             );
 
             const updatedSection = {
               ...section,
               content: updatedSectionContent,
             };
+
+            console.log('updatedSection',updatedSection)
 
             // # Add updated section and trailing sections to start row.
             startRow = [...startRow, updatedSection, ...row.slice(sectionIndex + 1)];
@@ -328,33 +345,22 @@ const EditorInput = ({
           prevSectionsContentLength += section.content.length;
         };
 
-        rowsResult = [...rowsResult, startRow];
+        rowsResult = [...rowsResult, startRow, ...trailingRows];
       };
 
       return rowsResult;
     };
 
-    // console.log('Test')
-    // console.log('result',
-    //   addContentToDocument(
-    //     [
-    //       [{style: Settings.defaultStyles, content: '123'},{style: Settings.defaultStyles, content: 'abc'},{style: Settings.defaultStyles, content: 'xqc'}],
-    //       [{style: Settings.defaultStyles, content: 'ddd'},{style: Settings.defaultStyles, content: ' 222'},{style: Settings.defaultStyles, content: 'd'}],
-    //     ],
-    //     11,
-    //     'ADD\nnew line'
-    //   )
-    // );
-
+    const selectionStartRowIndex = findRowIndex(documentFile.rows, selectionOnCapture.start)
+    const selectionEndRowIndex = findRowIndex(documentFile.rows, selectionOnCapture.end)
     let rowsFinal: DocumentFile['rows'] = [];
 
     // # Remove all content in between selection range.
     if (selectionOnCapture.start !== selectionOnCapture.end) {
-      const rowChangeIndex = findRowIndex(documentFile.rows, selectionOnCapture.start)
-
       for (const [rowIndex, row] of documentFile.rows.entries()) {
         const rowContentIndexStart = getRowContentIndexByBodySelectionIndex(documentFile.rows, selectionOnCapture.start, rowIndex);
         const rowContentIndexEnd = getRowContentIndexByBodySelectionIndex(documentFile.rows, selectionOnCapture.end, rowIndex);
+        const isSelectedRow = selectionStartRowIndex <= rowIndex && selectionEndRowIndex >= rowIndex;
 
         const rowFinal = removeContentFromRow(
           row,
@@ -376,29 +382,47 @@ const EditorInput = ({
           ];
         }
         // # Add new row.
-        else if (rowFinalContentLength || rowIndex === rowChangeIndex) {
+        else if (
+          rowFinalContentLength
+          || rowIndex === selectionStartRowIndex
+          || !isSelectedRow
+        ) {
           rowsFinal = [...rowsFinal, rowFinal];
         };
       };
     }
     // # Backspace input - remove one char from selection.
     else if (newContentLength === -1) {
-      const rowChangeIndex = findRowIndex(documentFile.rows, selectionOnCapture.start)
-      const rowsBeforeChange = documentFile.rows.slice(0, rowChangeIndex);
-      const rowsAfterChange = documentFile.rows.slice(rowChangeIndex + 1);
-      const rowToChangeContentLength = documentFile.rows[rowChangeIndex].reduce(
+      const selectionStartRowIndex = findRowIndex(documentFile.rows, selectionOnCapture.start);
+      const rowContentChangeIndex = getRowContentIndexByBodySelectionIndex(documentFile.rows, selectionOnCapture.start, selectionStartRowIndex);
+
+      const prevRow = documentFile.rows[selectionStartRowIndex - 1];
+      const rowsBeforeChange = documentFile.rows.slice(0, selectionStartRowIndex);
+      const rowsAfterChange = documentFile.rows.slice(selectionStartRowIndex + 1);
+      const rowToChangeContentLength = documentFile.rows[selectionStartRowIndex].reduce(
         (acc2, section) => acc2 + section.content, ''
       ).length;
 
-      // Remove changed row if no content
+      // * Remove changed row if no content
       if (!rowToChangeContentLength) {
         rowsFinal = [...rowsBeforeChange, ...rowsAfterChange];
       }
-      // Remove one character from row.
+      // * Move row sections to prev row.
+      else if (rowContentChangeIndex === 0 && prevRow) {
+        const prevRowUpdated = [
+          ...prevRow,
+          ...documentFile.rows[selectionStartRowIndex],
+        ];
+
+        const rowsBeforeChangeLastRemoved = rowsBeforeChange.filter((_, i) => i !== rowsBeforeChange.length - 1);
+        const rowsBeforeChangeUpdated = [...rowsBeforeChangeLastRemoved, prevRowUpdated];
+
+        rowsFinal = [...rowsBeforeChangeUpdated, ...rowsAfterChange];
+      }
+      // * Remove one character from row.
       else {
-        const rowContentChangeIndex = getRowContentIndexByBodySelectionIndex(documentFile.rows, selectionOnCapture.start, rowChangeIndex);
         const changedRow = removeContentFromRow(
-          documentFile.rows[rowChangeIndex],
+          documentFile.rows[selectionStartRowIndex],
           rowContentChangeIndex - 1,
           rowContentChangeIndex,
         );
@@ -409,8 +433,9 @@ const EditorInput = ({
     // # Remove no content.
     else {
       rowsFinal = [...documentFile.rows];
-    }
+    };
 
+    console.log('**** ->',rowsFinal)
 
     // Add new content.
     const updatedRows = addContentToDocument(rowsFinal, selectionOnCapture.start, newContent);
